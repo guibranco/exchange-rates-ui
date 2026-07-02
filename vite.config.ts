460cab3a731e112780dfbd3b8b1348d6670890e1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-07-02 23:20:37 +0100

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from "path";
 
 export default defineConfig({
-  base: "/exchange-rates-ui/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["lucide-react"],
+    exclude: ['lucide-react'],
   },
   resolve: {
     alias: {
